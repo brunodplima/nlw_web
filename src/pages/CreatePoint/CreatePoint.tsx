4b711059468461d0957693405ad2f2a20a6b9d72
Bruno Lima <brunodplima@gmail.com>
2020-06-04 12:20:44 -0300

    Enviar dados para a API
    Criar mecanismo para envio dos dados à API e criação do novo ponto de
    coleta.

--- src/pages/CreatePoint/CreatePoint.tsx
+++ src/pages/CreatePoint/CreatePoint.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import { LeafletMouseEvent } from 'leaflet'
@@ -39,6 +39,8 @@ const CreatePoint = () => {
   const [selectedCity, setSelectedCity] = useState('')
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0])
   const [selectedItems, setSelectedItems] = useState<number[]>([])
+
+  const history = useHistory()
 
   useEffect(() => {
     api.get('items').then(response => {
@@ -88,6 +90,32 @@ const CreatePoint = () => {
     )
   }
 
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+
+    const { name, email, whatsapp } = inputData
+    const uf = selectedUf
+    const city = selectedCity
+    const [latitude, longitude] = selectedPosition
+    const items = selectedItems
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items,
+    }
+
+    await api.post('points', data)
+
+    alert('Ponto de coleta criado com sucesso')
+    history.push('/')
+  }
+
   return (
     <div id="page-create-point">
       <div className="content">
@@ -99,7 +127,7 @@ const CreatePoint = () => {
           </Link>
         </header>
         <main>
-          <form>
+          <form onSubmit={handleSubmit}>
             <h1>Cadastro do <br /> ponto de coleta</h1>
 
             <fieldset>
